802b8c58b8c06552b925cb41ad47724539a228ae
MaxReimann <max.reimann@student.uts.edu.au>
2016-06-24 10:19:31 +1000

    remove unused class declaration

--- pointCloud.h
+++ pointCloud.h
@@ -39,33 +39,3 @@ protected:
 
 
 
-class InstancedPointCloud: public omega::EngineModule
-{
-public:
-	static InstancedPointCloud* createAndInitialize(cyclops::SceneManager* sceneMng, std::string modelName, std::string modelPath, std::string  modelExtension, float pointSize);
-	InstancedPointCloud(cyclops::SceneManager* sceneMng, std::string modelName, std::string modelPath, std::string  modelExtension, float pointSize);
-
-	void createInstancedGeometry( osg::Image* img,  osg::Image* colorImg, unsigned int numInstances );
-	bool readPointDataTxt( const std::string& file, std::vector<float>& imageData, osg::BoundingBox& boundBox) ;
-	bool readPointModel(const std::string& file, std::vector<float>& imageData, std::vector<unsigned char>& vertColorData, osg::BoundingBox& boundBox);
-	bool readPointData( const std::string& file, const std::string& extension, float pointSize);
-
-	virtual void initialize();
-		virtual void update(const cyclops::UpdateContext& context);
-	// virtual void update(const cyclops::UpdateContext& context);
-
-	cyclops::ModelGeometry* getModelGeometry();
-
-protected:
-	cyclops::Ref<cyclops::SceneManager> m_sceneManager;
-	// Ref<StaticObject> m_pointCloudObject;
-
-	cyclops::Ref<cyclops::ModelGeometry> m_modelGeom;
-	std::string m_modelName;
-	std::string m_modelPath;
-	std::string  m_modelExtension;
-	float m_pointSize;
-};
-
-
-
